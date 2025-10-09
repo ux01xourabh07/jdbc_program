43ca4f644991a52ac332916a6215b8fed364850e
Sourabh Raikwar <nexuscode07>
2025-10-09 20:20:44 +0530

    new code

--- src/main/java/org/example/exmaple8.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.w3c.dom.ls.LSOutput;
-
-public class exmaple8 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
